c1e600aeeb84f081e98e5435ba2ee56f78208161
student <student@WS-C07S208ZG1J0.local>
2018-09-11 10:44:49 -0500

    pg 23 we hooked up 2 buttons and wrote a simple class for on click

--- GeoQuiz/app/src/main/java/com/noahfranck/geoquiz/geoquiz/MainActivityQuizActivity.java
+++ GeoQuiz/app/src/main/java/com/noahfranck/geoquiz/geoquiz/MainActivityQuizActivity.java
@@ -1,13 +1,32 @@
 package com.noahfranck.geoquiz.geoquiz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivityQuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+    public MainActivityQuizActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                System.out.println("True button clicked");
+            }
+        });
     }
 }
