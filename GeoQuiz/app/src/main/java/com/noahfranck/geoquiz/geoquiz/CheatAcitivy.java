f92e1ab23d7cf8e14200ab1d9e232d46526897da
Noah Franck <franck.noahk@gmail.com>
2018-10-03 01:12:49 -0500

    Got used GUI to fix build error. Unsure what was inported.

---
+++ GeoQuiz/app/src/main/java/com/noahfranck/geoquiz/geoquiz/CheatAcitivy.java
@@ -0,0 +1,13 @@
+package com.noahfranck.geoquiz.geoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CheatAcitivy extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_cheat);
+	}
+}
