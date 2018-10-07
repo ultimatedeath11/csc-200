13eb277fb4af14f40ef108bd782e481a5dcdd949
Noah Franck <franck.noahk@gmail.com>
2018-10-06 19:22:54 -0500

    pg 104. CheatActivity now displays if the answer is true or false.

--- GeoQuiz/app/src/main/java/com/noahfranck/geoquiz/geoquiz/CheatAcitivy.java
+++ GeoQuiz/app/src/main/java/com/noahfranck/geoquiz/geoquiz/CheatAcitivy.java
@@ -4,9 +4,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CheatAcitivy extends AppCompatActivity {
 	private static final String EXTRA_ANSWER_IS_TRUE = "com.noahfranck.android.geoquiz.answer_is_true";
+
+	private boolean mAnswerIsTrue;
+	private TextView mAnswerTextView;
+	private Button mShowAnswerButton;
 
 	public static Intent newIntent(Context packageContext, boolean answerIsTrue){
 		Intent intent = new Intent(packageContext, MainActivityQuizActivity.class);
@@ -18,5 +25,19 @@ public class CheatAcitivy extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_cheat);
+
+		mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE,false);
+
+		mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
+		mShowAnswerButton.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				if(mAnswerIsTrue){
+					mAnswerTextView.setText(R.string.true_button);
+				}else{
+					mAnswerTextView.setText(R.string.false_button);
+				}
+			}
+		});
 	}
 }
